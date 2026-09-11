c136edbe3d19231a012123de134f5289c0b11cb1
Won-Dong-Soo <twodongsoo0516@gmail.com>
2026-09-11 23:40:06 +0900

    add Structure

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# This is a sample Python script.
-
-# Press <no shortcut> to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# imports
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
+#functions
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('wondongsoo')
+    print("this is solution of TSP. ")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
